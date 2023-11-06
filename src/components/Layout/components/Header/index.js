15baa30781151a323f1f0a3d558557c759640330
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2023-11-06 16:35:23 +0700

    CSS search

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,6 @@
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Header.module.scss';
 import images from '~/assets/images/index';
@@ -12,15 +11,23 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <img src={images.logo.default} alt="Tiktok" />
+                <div className={cx('logo')}>
+                    {/* Phải thêm default vào nó mới lấy đường dẫn còn nếu không nó chỉ lấy ra 1 object (images.logo) */}
+                    <img src={images.logo.default} alt="Tiktok" />
+                </div>
                 <div className={cx('search')}>
                     <input placeholder="Search accounts and videos" spellCheck={false} />
                     <button className={cx('clear')}>
                         <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
-
-                    <button className={cx('search-btn')}></button>
+                    {/* loading */}
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        {/* Search */}
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
                 </div>
+
                 <div className={cx('actions')}></div>
             </div>
         </header>
